f574591eb237d12bcd562e506df7e0143cc36991
Miodrag Rajkovic <rajkovic.miodrag@gmail.com>
2020-02-27 19:55:32 +0100

    Creates Navbar with dummy links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,55 @@
 import React from 'react';
 
 const Navbar = props => {
-	return <header className="main-header">HEADER</header>;
+	return (
+		<header className="main-header">
+			<nav className="navbar navbar-expand-md navbar-light bg-light">
+				<a className="navbar-brand" href="#">
+					Jokes
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav ml-auto">
+						<li className="nav-item active">
+							<a className="nav-link" href="#">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								New Joke
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Register
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Login
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Logout
+							</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+	);
 };
 
 export default Navbar;
